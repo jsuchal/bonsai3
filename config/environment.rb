1f7d9a1e5ae346af114c978dc0561bfb57baabce
Jan Suchal <johno@jsmf.net>
2010-08-31 13:51:29 +0200

    Initial import.

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Bonsai3::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bonsai3::Application.config.secret_token = 'c659b9f680524355383c5eb1c9a4d0d3fab29f76bacdbdf2f1c089cb50691cc28fe11a7bc92253217118d0e6758c5d713fe28696234b510e25f70d7cfad2181b'
