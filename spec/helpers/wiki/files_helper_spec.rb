bb0cc98fba28a1f309dce9137753a64f4d14ae91
Jan Suchal <johno@jsmf.net>
2010-09-17 21:53:54 +0200

    Dropped controller specs. Removed pending steps.

--- spec/helpers/wiki/files_helper_spec.rb
+++ spec/helpers/wiki/files_helper_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the Wiki::FilesHelper. For example:
-#
-# describe Wiki::FilesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe Wiki::FilesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
 end
