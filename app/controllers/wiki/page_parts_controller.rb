198aa5e23adbdb48c065f1f96359321df39783c0
hidden <peter.student@gmail.com>
2011-05-15 23:37:46 +0200

    Remove page part editor after deleting page part.

--- app/controllers/wiki/page_parts_controller.rb
+++ app/controllers/wiki/page_parts_controller.rb
@@ -1,11 +1,11 @@
 class Wiki::PagePartsController < ApplicationController
   def destroy
     begin
-      page_part = PagePart.find(params[:id])
-      current_revision = page_part.current_revision
-      revision = page_part.current_revision.create(:part_id => page_part, :author => current_user, :body => current_revision.body, :summary => current_revision.summary, :was_deleted => true)
-      page_part.current_revision = revision
-      page_part.save
+      @page_part = PagePart.find(params[:id])
+      current_revision = @page_part.current_revision
+      revision = @page_part.current_revision.create(:part_id => @page_part, :author => current_user, :body => current_revision.body, :summary => current_revision.summary, :was_deleted => true)
+      @page_part.current_revision = revision
+      @page_part.save
     rescue
       logger.error "Deletion of page part #{params[:id]} failed"
     end
